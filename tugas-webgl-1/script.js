f42f136a0bbd3e2afca2b9c23d62b3c635cc0c95
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-09-14 20:59:26 +0700

    create condition if webgl not support

--- tugas-webgl-1/script.js
+++ tugas-webgl-1/script.js
@@ -1,3 +1,13 @@
 const main = () => {
-  console.log("Hello World");
+  const canvas = document.querySelector("#my-canvas");
+  const gl = canvas.getContext("webgl");
+
+  if (!gl) {
+    console.log("WebGL not supported, falling back on experimental-webgl");
+    gl = canvas.getContext("experimental-webgl");
+  }
+
+  if (!gl) {
+    alert("Your browser does not support WebGL");
+  }
 };
