264803f3e59361600657702ae70134f52f7f63bf
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-09-14 21:50:35 +0700

    add createShader and createProgram function

--- tugas-webgl-1/script.js
+++ tugas-webgl-1/script.js
@@ -1,17 +1,45 @@
-const main = () => {
-  const canvas = document.querySelector("#my-canvas");
-  const gl = canvas.getContext("webgl");
+const canvas = document.querySelector("#my-canvas");
+const gl = canvas.getContext("webgl");
 
-  if (!gl) {
-    console.log("WebGL not supported, falling back on experimental-webgl");
-    gl = canvas.getContext("experimental-webgl");
-  }
+if (!gl) {
+  console.log("WebGL not supported, falling back on experimental-webgl");
+  gl = canvas.getContext("experimental-webgl");
+}
 
-  if (!gl) {
-    alert("Your browser does not support WebGL");
-  }
+if (!gl) {
+  alert("Your browser does not support WebGL");
+}
 
-  gl.clearColor(1.0, 0.65, 0.0, 1);
+// Clear the canvas
+gl.clearColor(225.0, 225.0, 225.0, 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
 
-  gl.clear(gl.COLOR_BUFFER_BIT);
+// create vertex shader and fragment shader function
+const createShader = (gl, typeShader, sourceShader) => {
+  let shader = gl.createShader(typeShader);
+  gl.shaderSource(shader, sourceShader);
+  gl.compileShader(shader);
+
+  let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
+  if (success) return shader;
+
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+};
+
+// create program function
+const createProgram = (gl, vertexShader, fragmentShader) => {
+  let program = gl.createProgram();
+
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  let success = gl.getProgramParameter(program, gl.LINK_STATUS);
+
+  if (success) return program;
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
 };
