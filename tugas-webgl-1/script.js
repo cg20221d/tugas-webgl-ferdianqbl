24fc35e3267cad8860065c1cc74efbcb46d68ce9
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-09-20 11:56:06 +0700

    create 0

--- tugas-webgl-1/script.js
+++ tugas-webgl-1/script.js
@@ -53,6 +53,7 @@ gl.useProgram(program);
 
 
 // ========= DRAW ========
+
 let LVertices = [
   0.1, 0.0,
   0.15, 0.0,
@@ -91,5 +92,26 @@ let AVertices = [
 ]
 
 // draw
-drawShape(gl, gl.TRIANGLES, LVertices);
+// 0
+drawShape(gl, gl.LINES, [-0.25, 0.1, -0.25, 0.4]);
+drawShape(gl, gl.LINES, [-0.25 + -0.15, 0.1, -0.25 + -0.15, 0.4]);
+
+drawShape(gl, gl.LINES, [-0.25 + -0.15, 0.4, -0.325, 0.1 + 0.4]); // tengah atas
+drawShape(gl, gl.LINES, [-0.25, 0.4, -0.325, 0.1 + 0.4]); // tengah atas
+
+drawShape(gl, gl.LINES, [-0.25 + -0.15, 0.1, -0.325, 0.0]); // tengah bawah
+drawShape(gl, gl.LINES, [-0.25, 0.1, -0.325, 0.0]); // tengah bawah
+
+drawShape(gl, gl.POINT, [-0.325, 0.1 + 0.15]); // titik tengah atas
+
+drawShape(gl, gl.LINES, [-0.325, 0.1, -0.4, 0.4]);
+drawShape(gl, gl.LINES, [-0.325, 0.1, -0.4 + 0.15, 0.4]);
+
+drawShape(gl, gl.LINES, [-0.325, 0.4, -0.4, 0.1]);
+drawShape(gl, gl.LINES, [-0.325, 0.4, -0.4 + 0.15, 0.1]);
+
+
+// A
 drawShape(gl, gl.TRIANGLES, AVertices);
+// L
+drawShape(gl, gl.TRIANGLES, LVertices);
